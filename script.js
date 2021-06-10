ee9cd57d00151a415633fb68789b7a14f85e7aba
DFarau <44142390+DFarau@users.noreply.github.com>
2021-06-10 02:50:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $('.submit').click(function(event) {
-        console.log("Clicked button")
 
         var email = $('.email').val()
         var subject = $('.subject').val()
@@ -9,19 +8,19 @@ $(document).ready(function() {
         statusElm.empty()
 
         if(email.length > 5 && email.includes('@') && email.includes('.')) {
-            statusElm.append('<div>Email is valid')
+            statusElm.append('')
         } else {
             event.preventDefault()
             statusElm.append('<div>Email is not valid')
         }
         if(subject.length >= 2) {
-            statusElm.append('<div>Subject is valid')
+            statusElm.append('')
         } else {
             event.preventDefault()
             statusElm.append('<div>Subject is not valid')
         }
         if(message.length >= 10) {
-            statusElm.append('<div>Message is valid')
+            statusElm.append('')
         } else {
             event.preventDefault()
             statusElm.append('<div>Message is not valid')
